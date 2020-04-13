083e2543689901f32ad16b6d9361630738527cad
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2020-04-12 17:48:03 -0700

    Issue#12 customize identity (#13)
    * Scaffolded all ASP.NET Identity pages
    
    * Simplified name of database
    
    * Changed required account and modified password requirements
    
    * Modified lockout options
    
    * Modified register page to allow username and email separately
    
    * Change login to require username instead of email
    
    * Remove external login providers from login page

---
+++ SpeakingInBits/Models/IdentityHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpeakingInBits.Models
+{
+    public static class IdentityHelper
+    {
+        public static void SetIdentityOptions(IdentityOptions options)
+        {
+            // Set sign in options
+            options.SignIn.RequireConfirmedEmail = false;
+            options.SignIn.RequireConfirmedPhoneNumber = false;
+
+            // Set password strength
+            options.Password.RequireDigit = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequiredLength = 8;
+            options.Password.RequireNonAlphanumeric = false;
+
+            // Set lockout options
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+            options.Lockout.MaxFailedAccessAttempts = 5;
+        }
+    }
+}
--- SpeakingInBits/Startup.cs
+++ SpeakingInBits/Startup.cs
@@ -12,6 +12,7 @@ using SpeakingInBits.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SpeakingInBits.Models;
 
 namespace SpeakingInBits
 {
@@ -30,7 +31,7 @@ namespace SpeakingInBits
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(IdentityHelper.SetIdentityOptions)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
