66d5d44354c0fd512d6dfd549f74018f03554651
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2020-04-14 20:14:57 -0700

    Add user to student role if they have student email (#16)

--- SpeakingInBits/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SpeakingInBits/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -13,12 +13,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using SpeakingInBits.Models;
 
 namespace SpeakingInBits.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class RegisterModel : PageModel
     {
+        private const string StudentEmail = "@student.cptc.edu";
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
@@ -79,6 +81,16 @@ namespace SpeakingInBits.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+
+                    if (user.Email.ToLower().EndsWith(StudentEmail))
+                    {
+                        var roleResult = await _userManager.AddToRoleAsync(user, IdentityHelper.Student);
+                        if (!roleResult.Succeeded)
+                        {
+                            _logger.LogInformation($"{user.UserName} not added to Student role");
+                        }
+                    }
+                    
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
