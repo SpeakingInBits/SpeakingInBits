c3bc8bdcd693f71aadb5994744e8542bb8ed070d
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2020-05-28 22:13:06 -0700

    Issue#4 instructors create lessons (#18)
    * Create VideoLesson model
    
    * Add VideoLesson dbset
    
    * Add migration for video lessons
    
    * Scaffold CRUD pages for instructor video lessons
    
    * Add link to instructor management pages
    
    * Use raw HTML output for VideoLesson embed code

---
+++ SpeakingInBits/Controllers/InstructorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SpeakingInBits.Models;
+
+namespace SpeakingInBits.Controllers
+{
+    [Authorize(Roles = IdentityHelper.Instructor)]
+    public class InstructorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
