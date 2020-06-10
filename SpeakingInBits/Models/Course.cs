a61a8db695b99f123a9c916fa2fcf768c9427634
Joseph Ortiz <joeprogrammer88@users.noreply.github.com>
2020-06-09 23:03:41 -0700

    Create course model

---
+++ SpeakingInBits/Models/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpeakingInBits.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Code { get; set; }
+
+        public List<Lesson> Lessons { get; set; }
+    }
+}
