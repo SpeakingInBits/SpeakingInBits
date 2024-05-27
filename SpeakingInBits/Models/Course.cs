179b9c1d537bdf1e470dfd6151e4ac65729ebeb0
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2024-05-27 11:36:02 -0700

    Create course and lesson models

---
+++ SpeakingInBits/Models/Course.cs
@@ -0,0 +1,25 @@
+﻿namespace SpeakingInBits.Models
+{
+    public class Course
+    {
+        /// <summary>
+        /// Primary key for the course
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The title of the course displayed to the user
+        /// </summary>
+        public required string Title { get; set; }
+
+        /// <summary>
+        /// The description of the course displayed to the user
+        /// </summary>
+        public required string Description { get; set; }
+
+        /// <summary>
+        /// The lessons that belong to the course
+        /// </summary>
+        public ICollection<Lesson> Lessons { get; set; } = [];
+    }
+}
---
+++ SpeakingInBits/Models/Lesson.cs
@@ -0,0 +1,43 @@
+﻿namespace SpeakingInBits.Models
+{
+    public class Lesson
+    {
+        /// <summary>
+        /// Primary key for the lesson
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The title of the lesson displayed to the user
+        /// </summary>
+        public required string Title { get; set; }
+
+        /// <summary>
+        /// The description of the lesson displayed to the user
+        /// </summary>
+        public required string Description { get; set; }
+
+        /// <summary>
+        /// The course that the lesson belongs to
+        /// </summary>
+        public required Course Course { get; set; }
+
+        /// <summary>
+        /// The foreign key for the course that the lesson belongs to
+        /// </summary>
+        public int CourseId { get; internal set; }
+    }
+
+    public class VideoLesson : Lesson
+    {
+        /// <summary>
+        /// The embed code of the video
+        /// </summary>
+        public required string VideoEmbedCode { get; set; }
+
+        /// <summary>
+        /// Duration of the video in minutes
+        /// </summary>
+        public int Duration { get; set; }
+    }
+}
